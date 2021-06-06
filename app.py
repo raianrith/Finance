5e08bd5f39b9e6145756eb788a3ee945122cf472
hamzaehsan97 <hamzaehsan75@gmail.com>
2021-06-06 01:34:36 -0500

    Check db functionality

--- app.py
+++ app.py
@@ -31,21 +31,28 @@ def sms_reply():
         if command == "weight" or command == "Weight":
             #save weight
             try:
-                resp.message("Your weight has been recorded 🙌")
-                cur = get_db().cursor()
+                
+                conn = get_db()
+                cur = conn.cursor()
                 weight_in_lbs = command_body
-                phone_number = 3125090234
+                weight_in_lbs = 100
+                phone_number = 200
                 cur.execute('INSERT INTO weights (id, weight) VALUES(?,?)',(phone_number, weight_in_lbs))
+                conn.commit()
+                resp.message("Your weight has been recorded 🙌")
+                
             except Exception as e:
                 print(e)
                 resp.message("Hmm, that was weird. Let me try to fix that. 🧰")
         elif command == "note" or command == "Note":
             #save note
             try:
-                cur = get_db().cursor()
+                conn = get_db()
+                cur = conn.cursor()
                 note = command_body
                 phone_number = 3125090234
                 cur.execute('INSERT INTO notes (id, note) VALUES(?,?)',(phone_number, note))
+                conn.commit()
                 resp.message("Your note has been saved 📝")
             except Exception as e:
                 print(e)
@@ -56,6 +63,18 @@ def sms_reply():
         resp.message("Looks like I am having some issues textie. Let's try later 🥺")
 
     return str(resp)
+
+@app.route("/get/weight", methods=['GET', 'POST'])
+def get_weight():
+    cur = get_db().cursor()
+    cur.execute('SELECT * from weights')
+    rows = cur.fetchall()
+    weights = ""
+    for row in rows:
+        weights = weights + "Phone number = "+str(row[1])+" weight = "+str(row[2]) + "\n"
+    print(weights)
+    return weights
+        
 
 
 def get_db():
