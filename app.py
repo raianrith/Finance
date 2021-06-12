c851e02743684c4e1d008b8e9abb9ee1c6ae6541
hamzaehsan97 <hamzaehsan75@gmail.com>
2021-06-12 00:00:30 -0500

    Jsonify results

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 from typing import Text
 from twilio.rest import Client
 from flask import Flask, Flask, request, session, g, redirect, url_for, abort, \
-     render_template, flash
+     render_template, flash, jsonify
 from twilio.twiml.messaging_response import MessagingResponse
 from flask_sqlalchemy import SQLAlchemy
 import datetime
@@ -10,7 +10,7 @@ import random
 from dotenv import load_dotenv
 import os
 from os.path import join, dirname
-
+from flask_marshmallow import Marshmallow
 
 
 app = Flask(__name__)
@@ -18,7 +18,7 @@ app = Flask(__name__)
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-ENV = 'prod'
+ENV = 'dev'
 if ENV == 'dev':
     app.debug = True
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DEV_DATABASE_URL")
@@ -26,6 +26,7 @@ else:
     app.debug=False
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://egvyaqyigaujuu:0f852e4602daf4f86ceb0919aa6510631585c97b4ecf2cf7ef9fae02516f6a38@ec2-54-158-232-223.compute-1.amazonaws.com:5432/d8k8274ljhv05'
 db = SQLAlchemy(app)
+marsh = Marshmallow(app)
 class Texties(db.Model):
     __tablename__='texties_table'
     id = db.Column(db.Integer, primary_key=True)
@@ -39,6 +40,13 @@ class Texties(db.Model):
         self.textie = textie
         self.textie_type = textie_type
         self.phone_number = phone_number
+
+class TextiesSchema(marsh.Schema):
+    class Meta:
+        fields = ('id','textie_type','textie','phone_number','created_date')
+
+texties_schema =  TextiesSchema( many = True)
+textie_schema = TextiesSchema()
 
 @app.route('/')
 def index():
@@ -90,64 +98,23 @@ def get_weight():
     return_string = " "
     try:
         all_texties = Texties.query.all()
-        results = [{
-            "type": textie.textie_type,
-            "from": textie.phone_number,
-            "textie": textie.textie,
-            "date": textie.created_date
-        } for textie in all_texties]
-        
-        for text in results:
-            text_type = str(text['type'])
-            phone = str(text['from'])
-            textie = str(text['textie'])
-            date = str(text['date'])
-            return_string = return_string + "<b>From:</b> " + phone + " <b>" + text_type + "</b>: " + textie + " <b>Date:</b> " + date + "<br> <br>"
+        result = texties_schema.dump(all_texties)
+        return jsonify(result)
     except Exception as e:
         print(e)
-        return e
-    
-    return return_string
+        return "error fetching weights"
     
 @app.route("/get/notes", methods=['GET', 'POST'])
 def get_notes():
     return_string = " "
     try:
         all_texties = Texties.query.all()
-        results = [{
-            "type": textie.textie_type,
-            "from": textie.phone_number,
-            "textie": textie.textie,
-            "date": textie.created_date
-        } for textie in all_texties]
-        
-        for text in results:
-            text_type = str(text['type'])
-            phone = str(text['from'])
-            textie = str(text['textie'])
-            date = str(text['date'])
-            return_string = return_string + "<b>From:</b> " + phone + " <b>" + text_type + "</b>: " + textie + " <b>Date:</b> " + date + "<br> <br>"
+        result = texties_schema.dump(all_texties)
+        return jsonify(result)
     except Exception as e:
         print(e)
-        return e
-    
-    return return_string
+        return "error fetching notes"
         
-
-
-# def get_db():
-#     db = getattr(g, '_database', None)
-#     if db is None:
-#         db = g._database = sqlite3.connect(DATABASE)
-#     return db
-
-# @app.teardown_appcontext
-# def close_connection(exception):
-#     db = getattr(g, '_database', None)
-#     if db is not None:
-#         db.close()
-
-
 
 if __name__ == "__main__":
     app.run()
